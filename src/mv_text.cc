c479193069392774e51ae55f30079b53889eff58
Czipperz <czipperz@gmail.com>
2015-12-09 16:34:24 -0800

    `mvsot()` will set `contents.waiting_for_desired` to false.

--- src/mv_text.cc
+++ src/mv_text.cc
@@ -7,6 +7,7 @@ namespace vick {
 namespace move {
 
 boost::optional< std::shared_ptr<change> > mvsot(contents& contents, boost::optional<int>) {
+    contents.waiting_for_desired = false;
     contents.x = 0;
     for(auto x : contents.cont[contents.y]) {
         if(x == ' ' || x == '\t') contents.x++;
