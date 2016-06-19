9bb8a9a424e4184c850ea18b33da727aabc9a13c
Czipperz <czipperz@gmail.com>
2016-06-19 10:37:10 -0700

    Don't bounds check < 0 for `mv()`
    This is unsafe when on the second half of a `move_t`

--- src/mv.cc
+++ src/mv.cc
@@ -11,12 +11,8 @@ namespace vick {
 namespace move {
 
 void mv(contents& contents, move_t y, move_t x) {
-    if (static_cast<move_ts>(y) < 0)
-        y = 0;
     if (y >= contents.cont.size())
         y = contents.cont.size() - 1;
-    if (static_cast<move_ts>(x) < 0)
-        x = 0;
     if (x >= contents.cont[y].size())
         x = contents.cont[y].size() - 1;
     contents.y = y;
