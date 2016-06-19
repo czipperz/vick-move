dd57b39db6eea9bd57041b33821311550483ec64
Czipperz <czipperz@gmail.com>
2016-06-19 10:49:18 -0700

    Fix testing same condition twice (copy paste)

--- test/mvcol_tests.cc
+++ test/mvcol_tests.cc
@@ -17,7 +17,7 @@ TEST_CASE("mvcol", "[mvcol]") {
 
     mvcol(contents, 10); // doesn't do anything
     CHECK(contents.y == 0);
-    CHECK(contents.y == 0);
+    CHECK(contents.x == 3);
 
     mvcol(contents, 0);
     CHECK(contents.y == 0);
